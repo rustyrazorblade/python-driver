089c99231945e8717a85b5e0565a8de9ee8e6db9
Jon Haddad <jon@jonhaddad.com>
2016-01-26 17:39:50 -0800

    placeholders

---
+++ tests/integration/cqlengine/test_table_descriptor.py
@@ -0,0 +1,43 @@
+# Copyright 2015 DataStax, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Tests the .table() descriptor functions as advertised
+"""
+
+from tests.integration.cqlengine.base import BaseCassEngTestCase
+from cassandra.cqlengine.models import Model
+from cassandra.cqlengine.management import sync_table, drop_table
+from cassandra.cqlengine.columns import Integer, Text
+
+from tests.integration import PROTOCOL_VERSION
+
+class TableCallTest(BaseCassEngTestCase):
+    class BucketTableTest(Model):
+        __table_name__ = "bucket_table_test"
+        id = Integer(primary_key=True)
+        value = Text()
+
+    def setUpClass(cls):
+        sync_table(cls.BucketTableTest)
+
+    def tearDownClass(cls):
+        drop_table(cls.BucketTableTest)
+
+    def test_create_queryset(self):
+        tmp = self.BucketTableTest.table("bucket_table_test_jan_2016")
+        # queryset should have a this table registered
+
+        
+
